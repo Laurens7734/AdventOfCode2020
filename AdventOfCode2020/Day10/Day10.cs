5af56d57b00b23fe1e60b381920168c293239453
Laurens <laurensmeijerink@gmail.com>
2020-12-10 20:41:26 +0100

    Rename part 2

---
+++ AdventOfCode2020/Day10/Day10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day10 : Day
+    {
+        List<string> data;
+
+        public Day10(List<string> d)
+        {
+            data = d;
+        }
+
+        public string Answer1()
+        {
+            return "all are valid";
+        }
+
+        public string Answer2()
+        {
+            return "";
+        }
+    }
+}
