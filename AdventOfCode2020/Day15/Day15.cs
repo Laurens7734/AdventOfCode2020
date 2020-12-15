2c38df92b1f26d11a23f81c97bdc3101cd68df4d
Laurens <laurensmeijerink@gmail.com>
2020-12-15 13:18:05 +0100

    Day 15

---
+++ AdventOfCode2020/Day15/Day15.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day15:Day
+    {
+        List<string> data;
+        public Day15(List<string> d)
+        {
+            data = new List<string>(d[0].Split(','));
+        }
+
+        public string Answer1()
+        {
+            Dictionary<int, int> spokenNumbers = new Dictionary<int, int>();
+            int nextnum = -1;
+            for(int i = 0; i < 2019; i++)
+            {
+                if (i < data.Count)
+                {
+                    int dataread = int.Parse(data[i]);
+                    if (spokenNumbers.ContainsKey(dataread))
+                    {
+                        nextnum = i - spokenNumbers[dataread];
+                        spokenNumbers[dataread] = i;
+                    }
+                    else
+                    {
+                        nextnum = 0;
+                        spokenNumbers.Add(dataread, i);
+                    }
+                }
+                else
+                {
+                    if (!spokenNumbers.ContainsKey(nextnum)){
+                        spokenNumbers.Add(nextnum, i);
+                        nextnum = 0;
+                    }
+                    else
+                    {
+                        int temp = i - spokenNumbers[nextnum];
+                        spokenNumbers[nextnum] = i;
+                        nextnum = temp;
+                    }
+                }
+            }
+            return nextnum.ToString();
+        }
+
+        public string Answer2()
+        {
+            //not really efficient but fast eanough for this one
+            Dictionary<int, int> spokenNumbers = new Dictionary<int, int>();
+            int nextnum = -1;
+            for (int i = 0; i < 29999999; i++)
+            {
+                if (i < data.Count)
+                {
+                    int dataread = int.Parse(data[i]);
+                    if (spokenNumbers.ContainsKey(dataread))
+                    {
+                        nextnum = i - spokenNumbers[dataread];
+                        spokenNumbers[dataread] = i;
+                    }
+                    else
+                    {
+                        nextnum = 0;
+                        spokenNumbers.Add(dataread, i);
+                    }
+                }
+                else
+                {
+                    if (!spokenNumbers.ContainsKey(nextnum))
+                    {
+                        spokenNumbers.Add(nextnum, i);
+                        nextnum = 0;
+                    }
+                    else
+                    {
+                        int temp = i - spokenNumbers[nextnum];
+                        spokenNumbers[nextnum] = i;
+                        nextnum = temp;
+                    }
+                }
+            }
+            return nextnum.ToString();
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -7,10 +7,10 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            DataToList datareader = new DataToList("InputData/Day14Data.txt");
+            DataToList datareader = new DataToList("InputData/Day15Data.txt");
             List<string> data = datareader.GetStringList();
 
-            Day d = new Day14(data);
+            Day d = new Day15(data);
             Console.WriteLine(d.Answer1());
             Console.WriteLine(d.Answer2());
             Console.ReadKey();
