086f10638b4050f6a5b58e32bd809ddb01ab8524
Laurens <laurensmeijerink@gmail.com>
2020-12-16 21:37:33 +0100

    Day 16

---
+++ AdventOfCode2020/Day16/Attribute.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    class Attribute
+    {
+        static List<Attribute> allAtributes = new List<Attribute>();
+
+        public string name;
+        List<Tuple<int, int>> ranges;
+        List<int> legalPositions;
+        public Attribute(string n, List<string> r)
+        {
+            name = n;
+            ranges = new List<Tuple<int, int>>();
+            foreach(string s in r)
+            {
+                string[] split = s.Split('-');
+                int low = int.Parse(split[0]);
+                int high = int.Parse(split[1]);
+                ranges.Add(Tuple.Create(low, high));
+            }
+            allAtributes.Add(this);
+        }
+
+        public bool IsValid(int i)
+        {
+            foreach(Tuple<int,int> t in ranges)
+            {
+                if (i >= t.Item1 && i <= t.Item2)
+                    return true;
+            }
+            return false;
+        }
+
+        public bool PositionFound()
+        {
+            if(legalPositions != null)
+                if (legalPositions.Count == 1)
+                    return true;
+            return false;
+        }
+
+        public int Position()
+        {
+            if (this.PositionFound())
+            {
+                return legalPositions[0];
+            }
+
+            return -1;
+        }
+
+        public void UpdateLegalPositions(List<int> ticket)
+        {
+            if(legalPositions == null)
+                legalPositions = new List<int>(Enumerable.Range(0, ticket.Count));
+ 
+            List<int> toRemove = new List<int>();
+            foreach(int i in legalPositions)
+                if (!IsValid(ticket[i]))
+                    toRemove.Add(i);
+
+            foreach (int j in toRemove)
+                legalPositions.Remove(j);
+
+            if(legalPositions.Count == 1)
+            {
+                int myposition = legalPositions[0];
+                allAtributes.ForEach(a => a.RemovePosition(myposition));
+                legalPositions.Add(myposition);
+            }
+                
+        }
+
+        public void RemovePosition(int i)
+        {
+            if (legalPositions.Contains(i)) { 
+                legalPositions.Remove(i);
+
+                if (legalPositions.Count == 1)
+                {
+                    int myposition = legalPositions[0];
+                    allAtributes.ForEach(a => a.RemovePosition(myposition));
+                    legalPositions.Add(myposition);
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode2020/Day16/Day16.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day16:Day
+    {
+        List<Attribute> attributes = new List<Attribute>();
+        List<int> myTicket = new List<int>();
+        List<List<int>> otherTickets = new List<List<int>>();
+        public Day16(List<string> d)
+        {
+            bool fase1done = false, fase2done = false;
+            foreach(string s in d)
+            {
+                if(!(fase1done || fase2done))
+                {
+                    if (s.Equals(""))
+                    {
+                        fase1done = true;
+                        continue;
+                    }
+                    else
+                    {
+                        string[] temp = s.Split(": ");
+                        string name = temp[0];
+                        List<string> ranges = new List<string>(temp[1].Split(" or "));
+                        Attribute a = new Attribute(name, ranges);
+                        attributes.Add(a);
+                    }
+                }
+                if(fase1done && !fase2done)
+                {
+                    if (s.Equals(""))
+                    {
+                        fase2done = true;
+                        continue;
+                    }
+                    else if(s.StartsWith('y'))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        myTicket = s.Split(',').Select(int.Parse).ToList<int>();
+                    }
+                }
+                if (fase2done)
+                {
+                    if (s.StartsWith('n'))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        otherTickets.Add(s.Split(',').Select(int.Parse).ToList<int>());
+                    }
+                }
+            }
+        }
+
+        public string Answer1()
+        {
+            int total = 0;
+            foreach(List<int> ticket in otherTickets)
+            {
+                foreach(int i in ticket)
+                {
+                    bool match = false;
+                    foreach(Attribute a in attributes)
+                    {
+                        if (a.IsValid(i))
+                            match = true;
+                    }
+                    if (!match)
+                        total += i;
+                }
+            }
+            return total.ToString();
+        }
+
+        public string Answer2()
+        {
+            List<List<int>> vailtTickets = new List<List<int>>();
+
+            foreach(List<int> ticket in otherTickets)
+            {
+                if (TicketValidation(ticket))
+                    vailtTickets.Add(ticket);
+            }
+
+            foreach(List<int> ticket in vailtTickets)
+            {
+                if (RefinePossision(ticket))
+                    break;
+            }
+
+            long result = 1;
+            foreach(Attribute a in attributes)
+            {
+                if (a.name.StartsWith("departure"))
+                    result *= myTicket[a.Position()];
+            }
+            return result.ToString();
+        }
+
+        bool TicketValidation(List<int> ticket)
+        {
+            foreach (int i in ticket)
+            {
+                bool match = false;
+                foreach (Attribute a in attributes)
+                {
+                    if (a.IsValid(i))
+                        match = true;
+                }
+                if (!match)
+                    return false;
+            }
+            return true;
+        }
+
+        bool RefinePossision(List<int> ticket)
+        {
+            int count = 0;
+            foreach(Attribute a in attributes)
+            {
+                if (a.PositionFound())
+                {
+                    count++;
+                    continue;
+                }
+                else
+                {
+                    a.UpdateLegalPositions(ticket);
+                }
+                    
+            }
+            if (count == attributes.Count)
+                return true;
+            return false;
+        }
+    }
+}
